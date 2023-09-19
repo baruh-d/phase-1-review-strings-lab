696ce70ae7c53b610e924c43923497592f6ffc99
baru-d <barudaniel6@gmail.com>
2023-09-19 15:52:36 +0300

    working with various string methods

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const truncatedCurrentUser = currentUser.substr(0, 1)
+
+// console.log(truncatedCurrentUser);
+
+
+// const welcomeMessage = "Welcome to Flatbook, " + currentUser; // concatenate with + operator
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = `Welcome, ${truncatedCurrentUser}!`;
+
+console.log(shortGreeting);
+
+
+
+
+
